3f69de7c0f9a2ddd7427790c31b061c545b84810
JFD-SNA <58977757+JFD-SNA@users.noreply.github.com>
2020-06-08 09:16:31 +0200

    Update header.js

--- src/js/header.js
+++ src/js/header.js
@@ -3,6 +3,6 @@
 
 
         $.fn.resultLicense = function resultLicense() {
-			return "YK7Ky3qsTtKnU6tlt0OlhCtCzn1fqfY5G6glaKZynkM//Je/jo0yQC1sQfoKwGUU";
+			return "RenseignerIciUneCleValide";
             }
 }( jQuery ));
